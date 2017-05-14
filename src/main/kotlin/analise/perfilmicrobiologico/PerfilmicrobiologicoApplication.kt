d5c406c95f9e88c8e5a114a01483d1c9f744e151
Laura Gomes <lauracristina185@hotmail.com>
2017-05-14 16:10:18 -0300

    Initial commit

---
+++ src/main/kotlin/analise/perfilmicrobiologico/PerfilmicrobiologicoApplication.kt
@@ -0,0 +1,11 @@
+package analise.perfilmicrobiologico
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class PerfilmicrobiologicoApplication
+
+fun main(args: Array<String>) {
+    SpringApplication.run(PerfilmicrobiologicoApplication::class.java, *args)
+}
